cc497b946b4e2269fcb04dc05d4f1aadf0a66d64
Denys Dronov <dronovdenis36@gmail.com>
2023-11-06 10:30:44 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp10;
+
+static void Main()
+{
+    Repository<int> intRepository = new Repository<int>();
+
+    intRepository.Add(1);
+    intRepository.Add(2);
+    intRepository.Add(3);
+    intRepository.Add(4);
+    intRepository.Add(5);
+
+    
+    Repository<int>.Criteria<int> isEven = x => x % 2 == 0;
+
+    List<int> evenNumbers = intRepository.Find(isEven);
+
+    Console.WriteLine("Парні числа:");
+    foreach (int number in evenNumbers)
+    {
+        Console.WriteLine(number);
+    }
+}
